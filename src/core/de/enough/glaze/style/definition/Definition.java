b8f80f51ea32e8e649f49b7cfe295b2b40002129
Ovidiu Iliescu <ovidiu.iliescu@enough.de>
2012-07-19 12:30:13 +0300

    Added licensing info.

--- src/core/de/enough/glaze/style/definition/Definition.java
+++ src/core/de/enough/glaze/style/definition/Definition.java
@@ -1,3 +1,16 @@
+/*
+ *
+ * Copyright: (c) 2012 Enough Software GmbH & Co. KG
+ *
+ * Licensed under:
+ * 1. MIT: http://www.opensource.org/licenses/mit-license.php
+ * 2. Apache 2.0: http://opensource.org/licenses/apache2.0
+ * 3. GPL with classpath exception: http://www.gnu.org/software/classpath/license.html
+ *
+ * You may not use this file except in compliance with these licenses.
+ *
+ */
+ 
 package de.enough.glaze.style.definition;
 
 import java.util.Enumeration;
